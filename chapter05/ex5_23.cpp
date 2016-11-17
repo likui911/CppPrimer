3412d2b11451be6b8f1356bf3eeaf95990ef6b16
unknown <>
2016-11-17 20:01:12 +0800

    chapter05

---
+++ chapter05/ex5_23.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    double result = 0;
+    cout << "pls input two oprands:" << endl;
+    cin >> a >> b;
+
+    result = (double)a / b;
+
+    cout << result << endl;
+    return 0;
+}
