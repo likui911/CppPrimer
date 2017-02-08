87f6e0c53f50c12b9c4fb0ae50a8e65175ebf831
likui911 <Likui_911@163.com>
2017-02-08 19:08:30 +0800

    chapter07

---
+++ chapter07/ex7_50.h
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+
+#ifndef EX_7_50_H
+#define EX_7_50_H
+
+/*
+* hide the Person’s implementation
+*/
+class Person
+{
+  public:
+    Person() = default;
+    Person(std::string &n, std::string &s) : address(s){};
+    // make the constructor explicit
+    explicit Person(std::istream &is) { read(is, *this); };
+
+    std::string getName() { return name; };
+    std::string getAddress() { return address; };
+
+    std::istream &read(std::istream &is, Person &p);
+    std::ostream &print(std::ostream &os, Person &p);
+
+  private:
+    std::string name;
+    std::string address;
+};
+
+std::istream &Person::read(std::istream &is, Person &p)
+{
+    is >> p.name >> p.address;
+    return is;
+}
+
+std::ostream &Person::print(std::ostream &os, Person &p)
+{
+    os << p.name << "" << p.address << std::endl;
+    return os;
+}
+#endif
