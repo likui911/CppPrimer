7e8467504de8548fceef60395631a729712a0a07
likui911 <Likui_911@163.com>
2017-10-18 12:30:27 +0800

    ex11_13

--- chapter11/ex11_13.cpp
+++ chapter11/ex11_13.cpp
@@ -4,15 +4,28 @@ program for the previous exercise. Write three versions of that program,
 creating the pairs in each way. Explain which form you think is easiest to
 write and understand, and why.
 */
+#include <iostream>
+#include <string>
 #include <utility>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-    //todo
-    pair<int,int> p1({{1,2},{3,4}});
-    pair<int,int> p2=make_pair(1,2);
-    pair<int,int> p3(1,2);
+    string s;
+    int i;
+    vector<pair<string, int>> vec;
+    while (cin >> s >> i)
+    {
+        //pair<string, int> p{s, i};
+        //pair<int,int> p2=make_pair(s,i);
+        vec.push_back({s,i});
+    }
+
+    for(auto &p:vec)
+    {
+        cout<<p.first<<" - "<<p.second<<endl;
+    }
     return 0;
 }
